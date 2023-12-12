68f5e28e55b88ded89c743c19439672ace972ac2
Weykon <36456814+weykon@users.noreply.github.com>
2023-12-12 13:41:54 +0800

    Create env.prepare.sh

---
+++ env.prepare.sh
@@ -0,0 +1,9 @@
+#/usr/bin bash
+python3 -m pip install -U pip
+
+# not necessary with higher version tensorflow now for me 
+# python3 -m pip install tensorflow
+
+python3 -m pip install tensorflow-macos==2.9.0
+
+python3 -m install tensorflow-metal==0.5.1
